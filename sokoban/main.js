5a537e71a97254b10f3cb134acbaf96642c58bb8
alkn203 <alkn203@gmail.com>
2020-03-06 00:48:39 +0900

    update

--- sokoban/main.js
+++ sokoban/main.js
@@ -1,5 +1,48 @@
 // グローバルに展開
 phina.globalize();
+// スプライトシート
+var SPRITE_SHEET = {
+  "tomapiko_ss": {
+    // フレーム情報
+    "frame": {
+      // 1フレームの画像サイズ（横）
+      "width": 64,
+      // 1フレームの画像サイズ（縦）
+      "height": 64,
+      // フレーム数（横）
+      "cols": 6,
+      // フレーム数（縦）
+      "rows": 3,
+    },
+    // アニメーション情報
+    "animations": {
+      // アニメーション名
+      "left": {
+        // フレーム番号範囲
+        "frames": [12,13,14],
+        // 次のアニメーション
+        "next": "left",
+        // アニメーション間隔
+        "frequency": 4,
+      },
+      "right": { 
+        "frames": [15,16,17], 
+        "next": "right", 
+        "frequency": 4, 
+      },
+      "up": { 
+        "frames": [9,10,11], 
+        "next": "up", 
+        "frequency": 4, 
+      },
+      "down": { 
+        "frames": [6,7,8], 
+        "next": "down",
+        "frequency": 4,
+      },
+    }
+  },
+};
 // アセット
 var ASSETS = {
   // 画像
@@ -8,83 +51,58 @@ var ASSETS = {
     'tomapiko': 'https://cdn.jsdelivr.net/gh/phinajs/phina.js@develop/assets/images/tomapiko_ss.png',
   },
   // スプライトシート
-  spritesheet: {
-    "tomapiko_ss": {
-      // フレーム情報
-      "frame": {
-        "width": 64, // 1フレームの画像サイズ（横）
-        "height": 64, // 1フレームの画像サイズ（縦）
-        "cols": 6, // フレーム数（横）
-        "rows": 3, // フレーム数（縦）
-      },
-      // アニメーション情報
-      "animations": {
-        "left": { // アニメーション名
-          "frames": [12,13,14], // フレーム番号範囲
-          "next": "left", // 次のアニメーション
-          "frequency": 4, // アニメーション間隔
-        },
-        "right": { 
-          "frames": [15,16,17], 
-          "next": "right", 
-          "frequency": 4, 
-        },
-        "up": { 
-          "frames": [9,10,11], 
-          "next": "up", 
-          "frequency": 4, 
-        },
-        "down": { 
-          "frames": [6,7,8], 
-          "next": "down",
-          "frequency": 4,
-        },
-      }
-    },
-  }
+  spritesheet: SPRITE_SHEET
 };
 // 定数
 var UNIT = 64;
 // キー情報配列
 var KEY_ARR = [['left', -1, 0],['right', 1, 0],['up', 0, -1],['down', 0, 1]];
-// マップデータ
-var MAP_DATA = [
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,3,3,3,3,3,0],
-  [0,3,3,3,3,1,1,1,3,0],
-  [0,3,2,1,1,1,2,1,3,0],
-  [0,3,1,3,1,3,1,3,3,0],
-  [0,3,1,1,1,1,1,3,0,0],
-  [0,3,3,3,3,3,3,3,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
+// ステージデータ
+var STAGE_DATA = [
+  {
+    // ステージ1
+    // マップデータ
+    map: [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,3,3,3,3,3,0],
+      [0,3,3,3,3,1,1,1,3,0],
+      [0,3,2,1,1,1,2,1,3,0],
+      [0,3,1,3,1,3,1,3,3,0],
+      [0,3,1,1,1,1,1,3,0,0],
+      [0,3,3,3,3,3,3,3,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0]],
+    // マップ当たり判定データ
+    collision: [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,1,1,1,1,1,0],
+      [0,1,1,1,1,0,0,0,1,0],
+      [0,1,0,0,0,0,0,0,1,0],
+      [0,1,0,1,0,1,0,1,1,0],
+      [0,1,0,0,0,0,0,1,0,0],
+      [0,1,1,1,1,1,1,1,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0]],
+    // 荷物の位置データ
+    baggage: [
+      {i: 4, j: 6},
+      {i: 4, j: 7}],
+    // プレイヤーの位置データ
+    player: {i: 4, j: 8},
+  },
 ];
-// マップ当たり判定データ
-var MAP_COLLISION = [
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,1,1,1,1,1,0],
-  [0,1,1,1,1,0,0,0,1,0],
-  [0,1,0,0,0,0,0,0,1,0],
-  [0,1,0,1,0,1,0,1,1,0],
-  [0,1,0,0,0,0,0,1,0,0],
-  [0,1,1,1,1,1,1,1,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0],
-];
-// 荷物の位置データ
-var BAG_LOCATION = [[4, 6], [4, 7]];
 /*
  * メインシーン
  */
@@ -100,16 +118,16 @@ phina.define("MainScene", {
       tileWidth: UNIT,
       tileHeight: UNIT,
       imageName: 'tile',
-      mapData: MAP_DATA,
-      collisionData: MAP_COLLISION,
+      mapData: STAGE_DATA[0].map,
+      collisionData: STAGE_DATA[0].collision,
     }).addChildTo(this);
     // プレイヤー作成・配置
     this.player = Player().addChildTo(this);
-    this.locateObject(this.player, 4, 8);
+    this.locateObject(this.player, STAGE_DATA[0].player.i, STAGE_DATA[0].player.j);
     // 荷物グループ
     this.baggageGroup = DisplayElement().addChildTo(this);
     // 荷物配置
-    this.locateBaggage(BAG_LOCATION);
+    this.locateBaggage(STAGE_DATA[0].baggage);
   },
   // 荷物配置
   locateBaggage: function(location) {
@@ -118,12 +136,14 @@ phina.define("MainScene", {
     location.each(function(elem) {
       // 荷物作成
       var baggage = Baggage().addChildTo(self.baggageGroup);
-      self.locateObject(baggage, elem[0], elem[1]);
+      self.locateObject(baggage, elem.i, elem.j);
     });
   },
   // オブジェクト配置用メソッド
   locateObject: function(obj, i, j) {
-    obj.setPosition(i * UNIT + UNIT / 2, j * UNIT + UNIT / 2);
+    var x = i * UNIT + UNIT / 2;
+    var y = j * UNIT + UNIT / 2;
+    obj.setPosition(x, y);
   },
   // 毎フレーム処理  
   update: function(app) {
@@ -139,7 +159,7 @@ phina.define("MainScene", {
       
     var key = app.keyboard;
     var array = [['left', -1, 0],['right', 1, 0],['up', 0, -1],['down', 0, 1]];
-    // 上下左右移動
+    // 移動判定
     KEY_ARR.each(function(elem) {
       var e0 = elem[0];
       var e1 = elem[1];
@@ -199,17 +219,21 @@ phina.define("MainScene", {
                    .call(function() {
                      var tile = map.checkTile(baggage.x, baggage.y);
                      baggage.frameIndex = tile === 2 ? 5 : 4;
+                     console.log(self.checkSpot());
                    }).play();
   },
-  // スポットに荷物があるかチェックする
+  // 全てのスポットに荷物があるか
   checkSpot: function() {
-    var map = this.map;
+    var result = true;
     
-    this.baggageGroup.children.each(function(baggage) {
-      if (map.checkTile(baggage.x, baggage.y) === 2) {
-        baggage.frameIndex = 5;
+    this.baggageGroup.children.some(function(baggage) {
+      // 色が変わっていない荷物があればNG
+      if (baggage.frameIndex !== 5) {
+        result = false;
+        return true;
       }
     });
+    return result;
   },
   // 指定位置に荷物があれば返す
   getBaggage: function(x, y) {
@@ -233,7 +257,7 @@ phina.define("Player", {
   // コンストラクタ
   init: function() {
     // 親クラス初期化
-    this.superInit('tomapiko', 64, 64);
+    this.superInit('tomapiko', UNIT, UNIT);
     this.moving = false;
     // スプライトにフレームアニメーションをアタッチ
     this.anim = FrameAnimation('tomapiko_ss').attachTo(this);
@@ -250,7 +274,7 @@ phina.define("Baggage", {
   // コンストラクタ
   init: function() {
     // 親クラス初期化
-    this.superInit('tile', 64, 64);
+    this.superInit('tile', UNIT, UNIT);
     // フレームインデックス指定
     this.frameIndex = 4;
   },
@@ -266,8 +290,6 @@ phina.main(function() {
     // アセット読み込み
     assets: ASSETS,
   });
-  // fps表示
-  //app.enableStats();
   // 実行
   app.run();
 });
